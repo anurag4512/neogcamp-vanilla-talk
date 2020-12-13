3828a79f3a518a9abd8f0cd8a4cc70e3e4675267
Anurag Kumar <anuragsingh4512@gmail.com>
2020-12-13 14:38:50 +0530

    querySelector imported

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtinput = document.querySelector("#txt-input");
 
+console.log(btnTranslate);
 function clickHandler(){
     console.log("clicked");
 }
